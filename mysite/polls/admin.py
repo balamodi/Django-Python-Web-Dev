2975084163c2987546da3a9d01c979776e9a592a
DR. B. MODI <modi_pas@yahoo.co.uk>
2018-08-29 14:51:47 +0100

    First Update

---
+++ mysite/polls/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from . models import Question, Choice
+# Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+admin.site.register(Question, QuestionAdmin)
+
+class ChoiceAdmin(admin.ModelAdmin):
+	fieldsets = [
+        (None,               {'fields': ['choice_text']}),
+        ('Votes', {'fields': ['votes']}),
+    ]
+    #list_display = ('choice_text', 'votes')
+admin.site.register(Choice, ChoiceAdmin)
